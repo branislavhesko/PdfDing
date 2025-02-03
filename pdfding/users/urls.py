93f710c0c1bf1dd7ebe6e12e02fdf114e0d52511
mrmn <188248364+mrmn2@users.noreply.github.com>
2025-02-03 14:13:09 +0100

    chore: remove unneeded files for helm chart

--- pdfding/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users.views import ChangeSetting, Delete, settings
-
-urlpatterns = [
-    path('settings', settings, name="profile-settings"),
-    path('delete', Delete.as_view(), name="profile-delete"),
-    path('change_setting/<field_name>', ChangeSetting.as_view(), name="profile-setting-change"),
-]
